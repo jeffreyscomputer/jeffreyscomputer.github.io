503187a64fbecdb1f74798bb2f25db835dcfbfee
Jeffrey <computer.jeffreycool@gmail.com>
2017-01-15 12:41:33 -0800

    changed units on cap touch page to dB

--- matlab/FBratio_gain.m
+++ matlab/FBratio_gain.m
@@ -6,17 +6,17 @@ Fopen = BW/ADC;
 Fbx = BW/100;
 Fb1 = BW;
 
-f = 1:10:BW*10;
-G = BW./(Fopen+f);
-H = BW./(Fbx+f);
-U = BW./(Fb1+f);
+f = logspace(-1,8);
+G = 20*log10(abs(BW./(j*Fopen+f)));
+H = 20*log10(abs(BW./(j*Fbx+f)));
+U = 20*log10(abs(BW./(j*Fb1+f)));
 
 zero = ones(size(f));
 
-loglog(f,G,'--','Color','k','Linewidth',1.5); hold on;
-loglog(f,H,'--','Color','b','Linewidth',1.5);
-loglog(f,U,'--','Color','r','Linewidth',1.5); hold off;
-axis([1 BW*10 .1 ADC*10])
+semilogx(f,G,'--','Color','k','Linewidth',1.5); hold on;
+semilogx(f,H,'--','Color','b','Linewidth',1.5);
+semilogx(f,U,'--','Color','r','Linewidth',1.5); hold off;
+axis([1 BW*10 -10 110])
 grid on
 grid minor
 
@@ -24,4 +24,4 @@ legend('b = 0', '0 < b < 1', 'b = 1')
 set(gcf,'Color','white')
 title('Feedback Ratio (b) and Bandwidth')
 xlabel('frequency');
-ylabel('Gain');
+ylabel('Gain (dB)');
